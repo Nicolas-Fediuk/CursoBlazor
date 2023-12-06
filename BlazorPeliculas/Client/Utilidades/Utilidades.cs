cd120040b660f5f0d086bbda368414761f25ffed
Nicolas <nicolasfediuk@gmail.com>
2023-12-05 22:02:27 -0300

    push

---
+++ BlazorPeliculas/Client/Utilidades/Utilidades.cs
@@ -0,0 +1,10 @@
+﻿namespace BlazorPeliculas.Client.Utilidades
+{
+    public class Utilidades
+    {
+        public static string Upper(string str)
+        {
+            return str.ToUpper();   
+        }
+    }
+}
